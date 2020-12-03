b6436b27bc22c603552656e7dd0018cff10bc9b0
Tielin Li <tielinlilili84@student.mitt.ca>
2020-12-03 14:19:58 -0600

    add onclick event for show-text btn

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 const btnShowText = document.querySelector(".btn-show-text"),
 btnEdit = document.querySelector("btn-edit"),
 btn = document.querySelector(".btn-remove");
+
+btnShowText.onclick = function(){
+}
