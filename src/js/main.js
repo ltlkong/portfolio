b27fcb5e03ef94a03b0071c8421d20a0f020ad6a
Tielin Li <tielinlilili84@student.mitt.ca>
2020-12-08 13:44:52 -0600

    make add note btn works

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
 const btnEdit = document.querySelector("btn-edit"),
-btnRem = document.querySelector(".btn-remove"),
+btnRem = document.querySelectorAll(".btn-remove"),
 btnAddList = document.querySelector(".btn-add-list");
 
-btnShowText.onclick = function(){
-  const text = document.querySelector("textarea");
+let id = 0;
+
+btnAddList.onclick = function(){
+  const aNoteHTML = `
+      <li data-id=`+id+`>
+        <input type="text" class="title" placeholder="Enter the title">
+        <textarea type="text" class="content" placeholder="Enter the content"></textarea>
+        <button class="btn-edit">Edit</button>
+        <button class="btn-remove">X</button>
+      </li>`;
+
+  id++;
+
+  const noteList = document.querySelector("ul");
+  noteList.insertAdjacentHTML("beforeend", aNoteHTML);
 
 }
